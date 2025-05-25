3160c2eb1cb40fc59c2ceb4d778db0b52ff1fcd3
Chukwuemelie Obumse <emelieobumse100@gmail.com>
2025-05-25 01:23:34 +0100

    feat: add page title for Zen Finance in the main application layout

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,10 +3,13 @@ import type { AppProps } from "next/app";
 import { AuthProvider } from "@/contexts/AuthContext";
 import { Toaster } from "sonner";
 import AuthGuard from "@/guards/auth-guard";
-
+import Head from "next/head";
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <div className="min-h-screen flex flex-col">
+      <Head>
+        <title>Zen Finance - International Payments</title>
+      </Head>
       <AuthProvider>
         <AuthGuard>
           <Component {...pageProps} />
